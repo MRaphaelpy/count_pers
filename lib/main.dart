1af04837d54d982d8deb5dad8bcb2920ed86b437
Marcos Rafael <89277533+MRaphaelpy@users.noreply.github.com>
2021-10-11 09:42:28 -0300

    imagem
    imagem adicionada ao fundo

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,56 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Column(
-      children: <Widget>[
-        Text(
-          "Pessoas: 0",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          "Pode Entar!",
-          style: TextStyle(color: Colors.white,
-           fontStyle: FontStyle.italic,
-           fontSize: 30.0),
-        )
-      ],
-    ),
-  ));
+      title: "Contador de Pessoas",
+      home: Stack(
+        children: [
+          Image.asset(
+            "assets/images/tor.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode Entar!!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
+            ],
+          ),
+        ],
+      )));
 }
