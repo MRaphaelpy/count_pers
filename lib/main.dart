6d4ed6d2cbb99694e34d213809b26625600095fc
Marcos Rafael <89277533+MRaphaelpy@users.noreply.github.com>
2021-10-11 11:37:07 -0300

    FinalBuild

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,99 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Contador de Pessoas",
-      home: Stack(
-        children: [
-          Image.asset(
-            "assets/images/tor.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Pessoas: 0",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      onPressed: () {},
-                      child: Text(
-                        "+1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _info = "Pode Entrar";
+
+  int _people = 0;
+
+  void _changePeople(int init) {
+    setState(() {
+      _people += init;
+
+      if (_people == -100) {
+        _info = "Cara c e mt loco ate o 100 man?";
+      } else if (_people == -200) {
+        _info = "E serio msm? vamo para vc jata no mundo inverso";
+      } else if (_people <= 10) {
+        _info = "Pode Entar";
+      } else if (_people == 50) {
+        _info = "Cara eu ja falei que nao cabe mais pare";
+      } else {
+        _info = "Lotado";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          "assets/images/tor.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas : $_people",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      onPressed: () {},
-                      child: Text(
-                        "-1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
                   ),
-                ],
-              ),
-              Text(
-                "Pode Entar!!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 30.0),
-              )
-            ],
-          ),
-        ],
-      )));
+                ),
+              ],
+            ),
+            Text(
+              _info,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        ),
+      ],
+    );
+  }
 }
